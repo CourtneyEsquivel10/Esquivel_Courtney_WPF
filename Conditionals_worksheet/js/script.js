70fc37021360e529cb290ab251b866db994c8e6d
Courtney Esquivel <courtneyesquivel@gmail.com>
2013-12-11 22:31:59 -0500

    conditionals weather

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,13 @@
+//Conditional
+//Hot Enough?
+//Is it hot enough to go to the beach?
+    var temp = 80;
+//if the temperature is less than 75..\\
+
+if(temp < 75){
+    //if its less than 75
+    console.log("We will go to the beach!");
+}else{
+    //if it's greater or equal to 75
+    console.log("We will go to the movies.");
+}
