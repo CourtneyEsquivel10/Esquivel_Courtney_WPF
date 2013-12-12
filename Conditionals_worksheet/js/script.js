b6f79039cbdd6529f3e083426e7f559422499bf8
Courtney Esquivel <courtneyesquivel@gmail.com>
2013-12-12 09:56:43 -0500

    ok

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,13 +1,16 @@
 //Conditional
-//Hot Enough?
-//Is it hot enough to go to the beach?
-    var temp = 80;
-//if the temperature is less than 75..\\
 
-if(temp < 75){
-    //if its less than 75
-    console.log("We will go to the beach!");
+//Is the temperature the same in both fahrenheit and celsius?
+    var fahrenheitTemp =0;
+    var celsiusTemp =132;
+    var higherDegree = false
+
+//if the temperature is 32 degrees celsius than it is 0 degree fahrenheit.
+
+if(fahrenheitTemp < celsiusTemp || higherDegree=== false){
+    //if it's 0 degrees Celsius than it is equal to 32 degrees fahrenheit
+
+    console.log("The temperature is the same in both celsius and fahrenheit!");
 }else{
-    //if it's greater or equal to 75
-    console.log("We will go to the movies.");
+    console.log("The temperature is not the same!!");
 }
