299bd82d5991bb5c9dd8e86635161d774b17f7cf
Courtney Esquivel <courtneyesquivel@gmail.com>
2013-12-12 21:49:05 -0500

    Conditonals_Personal

---
+++ Expressions_Personal/Conditionals_Personal/js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by courtneyesquivel on 12/12/13.
+ */
+//Conditional Logic - Ternary Operators
+
+var lights = 60;
+//if the are 60 count or higher you can use them
+/*if(lights > 60){
+    console.log("You can use them!");
+}else{
+ console.log("You can use them!");
+
+ }*/
+
+    (lights > 60) ? console.log("You can use them!") :  console.log("You can use them!");
