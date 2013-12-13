7dc9177588a94657f2a08f65b1d84ed50ddcc23f
Courtney Esquivel <courtneyesquivel@gmail.com>
2013-12-12 22:28:15 -0500

    Conditional Wacky

---
+++ Espressions_wacky/Conditionals_Wacky/js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by courtneyesquivel on 12/12/13.
+ */
+//Conditional
+
+var childName = Johnny;
+var anotherName = false;
+
+//if the name Johnny is not selected, print to console "Answer is incorrect!"
+//if the name Johnny is selected
+if(childName > anotherName){
+    //code performed if condition is true
+    console.log("Answer is correct") ;
+}else{
+    console.log("You are Wrong, Guess again!!");
+}
+
