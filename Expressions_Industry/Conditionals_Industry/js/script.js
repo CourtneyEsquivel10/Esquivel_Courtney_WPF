10c135f1e5567934b04d267c16b6af47aac43f1e
Courtney Esquivel <courtneyesquivel@gmail.com>
2013-12-12 21:30:45 -0500

    Conditionals Industry

---
+++ Expressions_Industry/Conditionals_Industry/js/script.js
@@ -0,0 +1,25 @@
+/**
+ * Created by courtneyesquivel on 12/12/13.
+ */
+//Conditional Logic_Logical Operators
+
+var studentAdvocate = 54000;
+var administrativeFees= 13500;
+var budgetLess= true;
+
+//if the student advocate is less than requested our administrative fees and budget fees will be less
+
+if(studentAdvocate < administrativeFees|| budgetLess ===true){
+    //code performed if condition is true
+    console.log("We are in the Budget!")
+
+}else{
+    console.log("Our Budget is less!")
+
+}
+
+
+
+
+
+
