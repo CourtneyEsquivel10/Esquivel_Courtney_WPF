c654f05e93a9382d68ab56704eb9637522eda9a3
Courtney Esquivel <courtneyesquivel@gmail.com>
2013-12-19 20:28:34 -0500

    Personal

---
+++ Functions_Personal/js/script.js
@@ -0,0 +1,9 @@
+//Functions - Parameters and Arguments
+
+function individualLights(){
+    var treeHeight =7;//average Christmas Tree
+    var lightStrand = 60//individualLights
+    var individualLights = treeHeight * 60;
+    console.log("There should be " + individualLights + " on a seven foot Christmas Tree .")
+}
+individualLights()
