5a65fee97082e196f1dd2ea9eac82a2e73c59c2b
Courtney Esquivel <courtneyesquivel@gmail.com>
2013-12-19 21:38:23 -0500

    fa

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,8 +2,8 @@
 //Functions -
 
 function johnny(){
-    var johnny = 5;
-    var otherNames = 0
+    var johnny = 5; // the only answer must be Johnny
+    var otherNames = 0// can not be any other name but Johnny
 
 
     console.log("Johnny is number " + johnny + " in the list. ")
