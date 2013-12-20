4d38940856d312ab6573e03eeb4fbd27804c2058
Courtney Esquivel <courtneyesquivel@gmail.com>
2013-12-19 20:42:59 -0500

    Industry take one

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,9 @@
+//Functions - Parameters and Arguments
+
+function remainingMoney(){
+    var contractAmount =54000;//Student Advocate Contract
+    var adminFees = 13500//Contract Administrator fees
+    var remainingMoney = contractAmount - adminFees;
+    console.log("The remaining " + remainingMoney + " is left for taxes, insurance and pay for advocates .")
+}
+remainingMoney()
