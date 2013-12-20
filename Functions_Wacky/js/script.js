9ebdb836e0e431d0024f34b6c5e5d036a8ddf9f8
Courtney Esquivel <courtneyesquivel@gmail.com>
2013-12-19 21:15:13 -0500

    wacky

---
+++ Functions_Wacky/js/script.js
@@ -0,0 +1,10 @@
+//Courtney Esquivel 12/19/2013
+//Functions - Parameters and Arguments
+
+function staffPay(){
+    var advocateContract =54000;//Full contract amount for Student advocates
+    var adminFees = 13500//fees for contract
+    var staffPay = advocateContract - adminFees;
+    console.log("There is " + staffPay + " remaining for staff pay including taxes and insurance.")
+}
+staffPay()
