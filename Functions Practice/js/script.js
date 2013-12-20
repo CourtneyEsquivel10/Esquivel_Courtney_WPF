9b1c5fb88aa6ecd0f8fa90f8405728d82063b835
Courtney Esquivel <courtneyesquivel@gmail.com>
2013-12-19 19:12:30 -0500

    sparky

--- Functions Practice/js/script.js
+++ Functions Practice/js/script.js
@@ -1,3 +1,9 @@
 //Functions - Parameters and Arguments
 
+function dogYears(){
+    var age =4;
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.")
 }
+dogYears();
+dogYears();
