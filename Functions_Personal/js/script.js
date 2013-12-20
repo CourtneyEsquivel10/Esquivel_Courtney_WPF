a887fd260581ab27c73885f7721f95caaaa5acfc
Courtney Esquivel <courtneyesquivel@gmail.com>
2013-12-19 20:31:11 -0500

    sentence change

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,6 +4,6 @@ function individualLights(){
     var treeHeight =7;//average Christmas Tree
     var lightStrand = 60//individualLights
     var individualLights = treeHeight * 60;
-    console.log("There should be " + individualLights + " on a seven foot Christmas Tree .")
+    console.log("Seven strands of lights are " + individualLights + " individual lightd on a seven foot Christmas Tree .")
 }
 individualLights()
